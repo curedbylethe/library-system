a9841476c7bdcd981ab60a569bbf61337f98b648
Parsa <106576103+curedbylethe@users.noreply.github.com>
2023-01-31 20:03:14 +0330

    chore: implement trim
    remove whitespace from start or end of strings

--- include/Library.cpp
+++ include/Library.cpp
@@ -5,6 +5,7 @@
 #include "Library.h"
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include <sstream>
 
 using namespace std;
@@ -243,6 +244,17 @@ namespace library {
                 }
             }
             return result;
+    }
+
+    string Library::trim(string &s) {
+        s.erase(s.begin(), find_if(s.begin(), s.end(), [](int ch) {
+            return !isspace(ch);
+        }));
+        s.erase(find_if(s.rbegin(), s.rend(), [](int ch) {
+            return !isspace(ch);
+        }).base(), s.end());
+        return s;
+
     }
 
     Library::Library() = default;
